59b8bd4dfa2bdc8d928ece385e43d6a5978eb552
Egor Badmaev <60363270+htmlprogrammist@users.noreply.github.com>
2022-12-04 11:25:50 +0300

    feat: implement logger

---
+++ Logger/Context.swift
@@ -0,0 +1,30 @@
+//
+//  Context.swift
+//  Logger
+//
+//  Created by Егор Бадмаев on 04.12.2022.
+//
+
+import Foundation
+
+struct Context {
+    private let file: String
+    private let function: String
+    private let line: Int
+    
+    var description: String {
+        return "\(extractFileName(from: file)): \(function): \(line)"
+    }
+    
+    init(file: String = #file,
+         function: String = #function,
+         line: Int = #line) {
+        self.file = file
+        self.function = function
+        self.line = line
+    }
+    
+    private func extractFileName(from path: String) -> String {
+        return path.components(separatedBy: "/").last ?? ""
+    }
+}
